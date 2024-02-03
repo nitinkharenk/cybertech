639d3ec97d610f6f369041cbbbf87d02271bef16
nitinkharenk <103874584+nitinkharenk@users.noreply.github.com>
2024-02-03 22:40:20 +0530

    Add files via upload

---
+++ app/ui/fonts.ts
@@ -0,0 +1,12 @@
+import { Inter, Roboto_Mono, Lusitana } from 'next/font/google'
+ 
+export const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+})
+ 
+export const lusitana = Lusitana({
+  subsets: ['latin'],
+  weight: '400',
+  display: 'swap',
+})
